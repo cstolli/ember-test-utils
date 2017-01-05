2fa2bfd2ee267e614a96c8cae1d8ad1e3551d6f8
Adam Meadows <adam.meadows@gmail.com>
2017-01-05 10:55:44 -0700

    Add ember-data helper

--- tests/unit/pods/demo/route-test.js
+++ tests/unit/pods/demo/route-test.js
@@ -3,26 +3,48 @@
  */
 import {expect} from 'chai'
 import {beforeEach, describe, it} from 'mocha'
+import sinon from 'sinon'
 
+import {returnPromiseWithArgs, stubStore} from 'dummy/tests/helpers/ember-test-utils/ember-data'
 import {route} from 'dummy/tests/helpers/ember-test-utils/setup-test'
 
-const test = route('demo')
+const test = route('demo', ['model:company'])
 describe(test.label, function () {
   test.setup()
 
-  let route
+  let route, sandbox, store, resolver
   beforeEach(function () {
+    sandbox = sinon.sandbox.create()
+    store = stubStore(this, sandbox)
     route = this.subject()
   })
 
   describe('.model()', function () {
     let model
     beforeEach(function () {
-      model = route.model()
+      resolver = returnPromiseWithArgs(store.findAll)
+      route.model().then((value) => {
+        model = value
+      })
     })
 
-    it('should return the username', function () {
-      expect(model.username).to.equal('tony.stark')
+    it('should find all the companies', function () {
+      expect(store.findAll).to.have.been.calledWith('company')
+    })
+
+    describe('when findAll resolves', function () {
+      beforeEach(function () {
+        resolver.resolve(['one', 'two', 'three'])
+        return resolver.promise
+      })
+
+      it('should return the proper username', function () {
+        expect(model.username).to.equal('tony.stark')
+      })
+
+      it('should return the proper companies', function () {
+        expect(model.companies).to.eql(['one', 'two', 'three'])
+      })
     })
   })
 })

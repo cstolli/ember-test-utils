0cef2551ee5a58f713776ddc05888cd935ebd1fc
Michael Carroll <juwara0@users.noreply.github.com>
2017-11-21 14:44:39 -0600

    update travis CI and pr-bumpber publishing scripts

--- .travis/maybe-bump-version.sh
+++ .travis/maybe-bump-version.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ "$TRAVIS_NODE_VERSION" != "6.11.0" ]
+then
+  echo "Skipping version bump for TRAVIS_NODE_VERSION ${TRAVIS_NODE_VERSION}"
+  exit 0
+fi
+
 if [ "$EMBER_TRY_SCENARIO" != "ember-default" ]
 then
   echo "Skipping version bump for EMBER_TRY_SCENARIO ${EMBER_TRY_SCENARIO}"
